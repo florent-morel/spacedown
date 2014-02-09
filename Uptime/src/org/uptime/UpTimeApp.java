dbc631f9d6b87e3cf45fe0e84547fd5944f1fd73
Florent Morel <flmorel.dev@gmail.com>
2014-02-09 16:44:20 +0100

    license

--- Uptime/src/org/uptime/UpTimeApp.java
+++ Uptime/src/org/uptime/UpTimeApp.java
@@ -20,6 +20,26 @@ import android.os.Environment;
 import android.preference.PreferenceManager;
 import android.widget.Toast;
 
+/**
+ * 
+ * This file is part of Uptime.
+ * 
+ * Uptime is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * Uptime is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with Uptime.  If not, see <http://www.gnu.org/licenses/>.
+ * 
+ * @author florent
+ *
+ */
 public class UpTimeApp extends Application {
 
 	/**
